5fd15964fabdaadf6bd78655a3c32919fdaaf6dc
DavidZ <davitzuroshvili@gmail.com>
2018-06-22 20:47:12 +0400

    Checkout

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-var VMs = document.getElementsByName('VM');
+function GetInput() {
+    var VMs = document.getElementsByName('Option');
 
-for (var i = 0, length = VMs.length; i < length; i++)
-{
-    if (VMs[i].checked)
+    for (var i = 0, length = VMs.length; i < length; i++)
     {
-        alert(VMs[i].value);
-        break;
-    }
+        if (VMs[i].checked)
+        {
+            sessionStorage("Choice",VMs[i].value);
+           // alert(VMs[i].value);
+            break;
+        }
+    }   
 }
