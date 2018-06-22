9e4833635e262203df0786611bbfa6b7816f6d1c
DavidZ <davitzuroshvili@gmail.com>
2018-06-22 21:38:17 +0400

    Checkout Page Transmition

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 function GetInput() {
     var VMs = document.getElementsByName('Option');
-
+    
     for (var i = 0, length = VMs.length; i < length; i++)
     {
         if (VMs[i].checked)
         {
-            sessionStorage("Choice",VMs[i].value);
-           // alert(VMs[i].value);
+            sessionStorage.setItem("Choice",VMs[i].value);
+            alert(VMs[i].value);
+            document.location.href = "checkout.html";
             break;
         }
     }   
